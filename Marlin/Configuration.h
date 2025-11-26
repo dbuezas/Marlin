300971bf2de389f0f6e0416fa018c57ccca90eab
David Buezas <david.buezas@gmail.com>
2025-11-26 08:19:01 +0100

    [UM2] quick heat/cool menu

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2558,17 +2558,11 @@
 //
 // Preheat Constants - Up to 10 are supported without changes
 //
-#define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 200
-#define PREHEAT_1_TEMP_BED     55
+#define PREHEAT_1_LABEL    "230/70"
+#define PREHEAT_1_TEMP_HOTEND 230
+#define PREHEAT_1_TEMP_BED     70
 #define PREHEAT_1_TEMP_CHAMBER  0
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
-
-#define PREHEAT_2_LABEL "PETG"
-#define PREHEAT_2_TEMP_HOTEND 230
-#define PREHEAT_2_TEMP_BED 75
-#define PREHEAT_2_TEMP_CHAMBER 0
-#define PREHEAT_2_FAN_SPEED 0  // Value from 0 to 255
 
 /**
  * @section nozzle park
