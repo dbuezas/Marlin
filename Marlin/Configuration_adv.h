1652382655b5d0e2e6fb4a0df65c61a8938ce94c
David Buezas <david.buezas@gmail.com>
2026-02-01 12:40:09 +0100

    [PR2] FT_MINIMUM_CRUISE_RATIO

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1228,6 +1228,9 @@
     #define FTM_POLY6_ACCELERATION_OVERSHOOT 1.875f // Max acceleration overshoot factor for POLY6 (1.25 to 1.875)
   #endif
 
+  // Minimum fraction of distance to spend at cruising speed (0.0f disables)
+  #define FTM_MINIMUM_CRUISE_RATIO 0.5f
+
   /**
    * Advanced configuration
    */
