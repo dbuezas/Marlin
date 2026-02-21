effbfb617ca6835a6aa7fb2bed620ef48394f558
sebastian <s@ebast.ian>
2026-02-22 00:02:26 +0100

    fix

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1826,7 +1826,7 @@
 
   #define SD_MENU_CONFIRM_START             // Confirm the selected SD file before printing
 
-  //#define NO_SD_AUTOSTART                 // Remove auto#.g file support completely to save some Flash, SRAM
+  #define NO_SD_AUTOSTART                 // Remove auto#.g file support completely to save some Flash, SRAM
   //#define MENU_ADDAUTOSTART               // Add a menu option to run auto#.g files
 
   #define ONE_CLICK_PRINT                 // Prompt to print the newest file on inserted media
